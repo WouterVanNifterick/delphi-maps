9d713e99002e4c29c1622f30f595c7c446005ceb
woutervannifterick <woutervannifterick@9050cbe2-e668-db9c-a8f9-121f7f96a0c8>
2010-10-20 03:39:11 +0000

    Geocoder Demo

---
+++ trunk/demos/DemoGeoCoder/DemoGeoCoder.dpr
@@ -0,0 +1,14 @@
+program DemoGeoCoder;
+
+uses
+  Forms,
+  uFrmMain in 'uFrmMain.pas' {frmMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+end.
